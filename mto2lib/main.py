5b3cac2ba18b8b66ab2e24d59513dda916f47cca
farshad <faezi.h.m@gmail.com>
2025-10-06 12:33:40 +0200

    Refactored

--- mto2lib/main.py
+++ mto2lib/main.py
@@ -1,11 +1,14 @@
 from mto2lib import io_utils
 from mto2lib.parser import make_parser
 from mto2lib.validators import validate_crop_coordinates
+import os
 
 
 def setup():
 
     arguments = make_parser().parse_args()
+
+    os.makedirs(arguments.output_path, exist_ok=True)
 
     crop_coords = None
 
@@ -13,6 +16,18 @@ def setup():
         crop_coords = validate_crop_coordinates(arguments.crop)
 
     image, header = io_utils.read_image_data(arguments.file_path, crop_coords)
+
+    if arguments.crop:
+
+        base_name = os.path.splitext(os.path.basename(arguments.file_path))[0]
+        crop_str = arguments.crop.replace(',', '_').replace(' ', '')
+        cropped_output_path = os.path.join(
+            arguments.output_path,
+            f"{base_name}_crop_{crop_str}.fits"
+        )
+
+        io_utils.save_fits_with_header(image, header, cropped_output_path)
+        print(f"Saved cropped original image to: {cropped_output_path}")
 
     return image, header, arguments
 
